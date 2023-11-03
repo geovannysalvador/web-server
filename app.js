83ea81dab6aadd2cdbce56297536dd6e7a574309
GeovannyTS <yowtecum@gmail.com>
2023-11-03 17:30:25 -0600

    Use send file, send & send static data

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
+const port = 8080;
+
+//hacer publica la carpeta public
+//servir contenido estatico.
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Pagina inicial')
-})
+});
 
 app.get('/saludo', (req, res) => {
     res.send('Hola')
-  })
+});
 
-  app.get('+', (req, res) => {
-    res.send('404 | page not found')
-  })
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+});
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
