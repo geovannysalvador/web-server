9794753fa420e876c76cdce1ebf06a93ecbd6274
GeovannyTS <yowtecum@gmail.com>
2023-11-03 16:59:46 -0600

    Request & Response

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http')
 
 http.createServer((req, res) =>{
-    res.write('Primera prueba');
+    
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+
+
+
+    res.write('id, nombre\n');
+    res.write('1, Juan\n');
+    res.write('2, Pedro\n');
+    res.write('3, Luna\n');
+    res.write('4, Javier\n');
+    res.write('5, Raul\n');
+    res.write('6, Fernando\n');
     res.end();
 })
 .listen(8080);
